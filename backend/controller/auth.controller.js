5dec30347cd9e7606ec3e863da186cf3cb8665af
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-10-04 10:25:56 +0700

    <setup> : setup backend

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+    res.send("Sign up is called");
+}
+export const login = async (req, res) => {
+    res.send("Login is called");
+}
+export const logout = async (req, res) => {
+    res.send("Logout is called");
+}
