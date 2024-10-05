5c5c38620a33b59eaa6152b5cf550ba9d89a2932
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-10-05 19:24:44 +0700

    <create>: refresh token routes

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -125,3 +125,43 @@ export const logout = async (req, res) => {
         })
     }
 }
+
+export const refreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) {
+            return res.status(401).json({
+                message: "no refresh token provided"
+            })
+        }
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const storedToken = await redis.get(`refreshToken:${decoded.user_id}`);
+
+        if (storedToken !== refreshToken) {
+            return res.status(401).json({
+                message: "Invalid refresh token"
+            })
+        }
+
+        const accessToken = jwt.sign({ userId: decoded.user_id }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "15m"
+        })
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 60 * 1000
+        })
+        res.json({
+            message: "Refresh token successfully"
+        })
+    } catch (error) {
+        console.log("Error refreshing token", error.message);
+        res.status(500).json({
+            message: "Server error", error: error.message
+        })
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import {signup, login, logout} from "../controller/auth.controller.js"
+import {signup, login, logout, refreshToken} from "../controller/auth.controller.js"
 
 const router = express.Router();
 
 router.post("/signup", signup)
 router.post("/login", login)
-router.post("/logout", logout)
+router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
 
 export default router
