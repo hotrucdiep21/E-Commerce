39b7086066676b72a1ae291acc04fa3ddd8c5214
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-10-05 15:23:53 +0700

    <create>: refeshToken and accessToken

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,77 @@
+import { redis } from "../lib/redis.js";
+import User from "../model/user.model.js";
+import jwt from "jsonwebtoken";
+
+console.log(process.env.REFRESH_TOKEN_SECRET);
+console.log(process.env.ACCESS_TOKEN_SECRET);
+
+const generateToken = (user_id) => {
+    const accessToken = jwt.sign({ user_id }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m"
+    })
+
+    const refreshToken = jwt.sign({ user_id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d"
+    })
+    return { accessToken, refreshToken }
+}
+
+const storeRefreshToken = async (user_id, refreshToken) => {
+    await redis.set(`refreshToken:${user_id}`, refreshToken, "EX", 7 * 24 * 60 * 60);
+}
+
+const setCookie = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, // Chỉ cho phép truy cập cookie qua HTTP, bảo vệ chống XSS.
+        secure: process.env.NODE_ENV === "production", //Chỉ gửi cookie qua HTTPS khi ứng dụng đang chạy trong môi trường production.
+        sameSite: "strict", //Bảo vệ chống lại các tấn công Cross-Site Request Forgery (CSRF).
+        maxAge: 15 * 60 * 1000
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+}
+
 export const signup = async (req, res) => {
-    res.send("Sign up is called");
+
+    const { name, email, password } = req.body;
+    try {
+
+        console.log(name, email, password)
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                message: "User already exists"
+            })
+        }
+
+        const user = await User.create({ name, email, password });
+
+        //authentication
+        const { accessToken, refreshToken } = generateToken(user._id);
+        await storeRefreshToken(user._id, refreshToken);
+
+        setCookie(res, accessToken, refreshToken);
+
+        res.status(201).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role 
+            },
+            message: "User created successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
 }
 export const login = async (req, res) => {
     res.send("Login is called");
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
-import  authRoutes  from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(express.json()) //allow json data to be sent to the server
 app.use("/api/auth", authRoutes)
+
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT)
+
+    connectDB();
 })
