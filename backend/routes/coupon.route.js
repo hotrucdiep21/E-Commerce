f1bb00581dae71d23913b6cb06aab959188841ab
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-10-08 11:46:01 +0700

    <create>: coupon features - additional

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {getCoupon} from "../controller/coupon.controller.js";
+import {getCoupon, validateCoupon} from "../controller/coupon.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.get("/", protectRoute, getCoupon);
+router.get("/validate", protectRoute, validateCoupon);
 
 export default router
