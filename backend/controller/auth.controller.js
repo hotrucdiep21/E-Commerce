13d3a676f9da7eaf952b5229b84d7cbcf2e713b0
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-10-11 14:10:56 +0700

    <add>: add route get profile

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -167,5 +167,12 @@ export const refreshToken = async (req, res) => {
 }
 
 export const getProfile = async (req, res) => {
-    
+    try {
+        res.json(req.user)
+    } catch (error) {
+        console.log("Error getting profile", error.message);
+        res.status(500).json({
+            message: "Server error", error: error.message
+        })
+    }
 }
